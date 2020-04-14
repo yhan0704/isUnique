fd261c9a70805557b167efdbddb52e5526f889ab
github username <github email address>
2020-04-14 12:59:27 -0400

    two solutions

--- isUnique.js
+++ isUnique.js
@@ -1,15 +1,31 @@
 //if sting is all unique characters
 //what if I cannot use additionoal daata strcture
+
+//solution1
+// var allUniqueChars = function(string) {
+//   for(let i = 0; i < string.length; i++){
+//       for(let j=i+1; j < string.length; j++){
+//           if(string[i] === string[j]){
+//               return false
+//           }
+//       }
+//   }
+//   return true
+// }
+
+//solution2
 var allUniqueChars = function(string) {
-  for(let i = 0; i < string.length; i++){
-      for(let j=i+1; j < string.length; j++){
-          if(string[i] === string[j]){
-              return false
-          }
-      }
+    // let splitString = string.split("")
+    for(let i = 0; i < string.length; i++){
+        let newArray = [...string]
+        let word = newArray.slice(i,i+1)
+        let leftword = newArray.slice(i+1)
+        if(leftword.includes(word[0])){
+            return false
+        }
+    }
+    return true
   }
-  return true
-}
 
 /* TESTS */
 console.log(allUniqueChars('abcd'), 'true');
